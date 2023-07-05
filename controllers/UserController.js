e6b7c30c97beccc386406dc6c143221aa2a215fc
jhosuapp <jhosuapvll@gmail.com>
2023-07-04 20:14:13 -0500

    feat: remove tailwind config and add sass config

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,12 @@
 const formLogin = (req, res)=>{
     res.render('auth/login', {
-        
+        page: 'Iniciar sesión'
     });
 }
 
 const formRegister = (req, res)=>{
     res.render('auth/register', {
-
+        page: 'Crear cuenta'
     });
 }
 
