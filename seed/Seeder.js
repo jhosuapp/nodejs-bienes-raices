db9ec80d776daa322ef32400bb0ac25090ef2259
jhosuapp <jhosuapvll@gmail.com>
2023-08-22 16:57:48 -0500

    feat: create script for delete tables

--- seed/Seeder.js
+++ seed/Seeder.js
@@ -7,6 +7,7 @@ import PricesModel from '../models/PricesModel.js';
 //CONEXION A BD
 import dataBase from '../config/DataBase.js';
 
+//CREAR TABLAS ESTATICAS
 const importData = async ()=>{
     try {
         //AUTENTICAR
@@ -14,15 +15,30 @@ const importData = async ()=>{
         //GENERAR COLUMNAS
         await dataBase.sync();
         //INSERTAR DATOS
-        await CategoriesModel.bulkCreate(CategoriesSeeder);
-        await PricesModel.bulkCreate(PricesSeeder);
+        await Promise.all([
+            CategoriesModel.bulkCreate(CategoriesSeeder),
+            PricesModel.bulkCreate(PricesSeeder)
+        ]);
         process.exit();
     }catch(err) {
-        console.log(err);
+        console.log(err + 'Error create');
         process.exit(1);
     }
 }
 
-if(process.argv[2] === "-i"){
-    importData();
+//ELIMINAR TABLAS ESTATICAS
+const deleteData = async()=>{
+    try{
+        await dataBase.sync({force: true});
+        process.exit();
+    }catch(err) {
+        console.log(err + 'Error delete');
+        process.exit(1);
+    }
 }
+
+//FUNCION PARA CREAR TABLAS
+process.argv[2] === "-i" && importData();
+
+//FUNCION PARA ELIMINAR TABLAS
+process.argv[2] === "-d" && deleteData();
